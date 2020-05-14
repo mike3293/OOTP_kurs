91627f21648e66849ef0ad2518a3ed353e8f5a8f
Mikhail Gorodilov <Mikhail.Gorodilov@itechart-group.com>
2020-05-14 16:46:00 +0300

    Add email validation

---
+++ WpfClient/WpfClient/Validations/EmailRule.cs
@@ -0,0 +1,31 @@
+﻿using DataAnnotations = System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Windows.Controls;
+
+namespace WpfClient.Validations
+{
+    public class EmailRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (value != null)
+            {
+                string input = value as string;
+
+                if (ValidateEmail(input))
+                {
+                    return new ValidationResult(true, null);
+                }
+            }
+
+            return new ValidationResult(false, "Email is not valid");
+        }
+
+        private bool ValidateEmail(string email)
+        {
+            var attribute = new DataAnnotations.EmailAddressAttribute();
+
+            return attribute.IsValid(email);
+        }
+    }
+}
